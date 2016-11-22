9fbf217333b24acdfd89e5631f2fc96c3f43dc86
Jacek Podkanski <jacek.podkanski@clarussoftware.co.uk>
2016-11-22 16:03:00 +0000

    basic text search works

--- log-search.rb
+++ log-search.rb
@@ -1,43 +1,64 @@
 require 'pg_query'
 require 'pry'
 
-def log_load(fh)
-  lines = fh.read.lines
-  # binding.pry
-  puts   lines[0]
+class LogData
+  attr_reader :prelude, :examples
 
-  prelude = []
-  examples = []
-
-  li = 0
-  parts = []
-  lines.each do |l|
-    # beginning of example
-    if l[0,3] == '==='
-      if prelude.empty?
-        prelude << parts
-      else
-        examples << parts
-      end
-      parts = []
-      parts << l
-    else
-      parts << l
-    end
-    li += 1
+  def initialize
+    @prelude = []
+    @examples = []
   end
-  examples << parts
 
-  binding.pry
-  [prelude, examples]
-end
+  def log_load(fh)
+    lines = fh.read.lines
+    # binding.pry
+    puts   lines[0]
 
-def sesarch(s)
+    @prelude = []
+    @examples = []
 
+    li = 0
+    parts = []
+    lines.each do |l|
+      # beginning of example
+      if l[0,3] == '==='
+        if prelude.empty?
+          @prelude << parts
+        else
+          @examples << parts
+        end
+        parts = []
+        parts << l
+      else
+        parts << l
+      end
+      li += 1
+    end
+    @examples << parts
+  end
+
+  def search(s)
+    @examples.each do |e|
+      found = []
+      e.each do |el|
+        if el.index(s)
+          found << el
+        end
+      end
+      if found.any?
+        puts e[0]
+        puts e[1]
+        found.each do |l|
+          puts l
+        end
+      end
+    end
+  end
 end
 
 # ----------------------------------------------
 def loader
+  lg = LogData.new
   path = ARGV[0]
   table = ARGV[1]
   if path.nil?
@@ -47,11 +68,12 @@ def loader
   elsif File.exist?(path)
     puts "File exists, going to load it"
     fh = File.open(path)
-    s = log_load(fh)
-    search(s)
+    lg.log_load(fh)
+    lg.search(table)
   else
     puts "File #{path} does not exist"
   end
 end
+
 
 loader
